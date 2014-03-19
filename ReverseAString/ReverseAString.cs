efad889d02dd7a842dbffde255e64cb81d8ac608
Murray Christopherson <murraychristopherson@gmail.com>
2014-03-19 11:43:09 -0400

    Added first interview question: ReverseAString with tests.

---
+++ ReverseAString/ReverseAString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roadrunner.Interview
+{
+    public class ReverseAString
+    {
+        public static char[] Reverse(char[] str)
+        {
+            char[] retVal = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                retVal[i] = str[str.Length - i - 1];
+            }
+            return retVal;
+        }
+
+        public static char[] ReverseRecursive(char[] str)
+        {
+            if (str.Length < 2)
+            {
+                return str;
+            }
+            else
+            {
+                char[] retVal = new char[str.Length];
+                char[] smallCopy = new char[str.Length - 1];
+                for (int i = 0; i < str.Length - 1; i++)
+                {
+                    smallCopy[i] = str[i];
+                }
+                retVal[0] = str[str.Length - 1];
+                char[] reversed = ReverseRecursive(smallCopy);
+                for (int i = 0; i < reversed.Length; i++)
+                {
+                    retVal[i + 1] = reversed[i];
+                }
+                return retVal;
+            }
+        }
+    }
+}
---
+++ ReverseAStringTests/UnitTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseAStringTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        private static readonly string inputStr = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        [TestMethod]
+        public void TestReverse()
+        {
+            char[] input = inputStr.ToCharArray();
+            char[] expected = new char[input.Length];
+            Array.Copy(input, expected, input.Length);
+            Array.Reverse(expected);
+
+            char[] retVal = Roadrunner.Interview.ReverseAString.Reverse(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], retVal[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestReverseRecursive()
+        {
+            char[] input = inputStr.ToCharArray();
+            char[] expected = new char[input.Length];
+            Array.Copy(input, expected, input.Length);
+            Array.Reverse(expected);
+
+            char[] retVal = Roadrunner.Interview.ReverseAString.ReverseRecursive(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], retVal[i]);
+            }
+        }
+    }
+}
