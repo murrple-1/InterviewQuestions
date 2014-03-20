d98420869b0375fd709c6b5501f2cd041ffdf1a7
Murray Christopherson <murraychristopherson@gmail.com>
2014-03-20 17:35:18 -0400

    Added Median Finder and Unit Tests.

---
+++ MedianFinder/MedianFinder.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roadrunner.Interview.MedianFinder
+{
+    public class MedianFinder
+    {
+        public static int FindMedian(int[] data)
+        {
+            return Select(data, data.Length / 2);
+        }
+
+        public static int Select(int[] data, int k)
+        {
+            return Select(data, 0, data.Length - 1, k);
+        }
+
+        private static int Select(int[] data, int left, int right, int k)
+        {
+            while (true)
+            {
+
+                if (right <= left + 1)
+                {
+
+                    if (right == left + 1 && data[right] < data[left])
+                    {
+                        Swap(data, left, right);
+                    }
+
+                    return data[k];
+
+                }
+                else
+                {
+
+                    int middle = (int) ((uint) (left + right) >> 1);
+                    Swap(data, middle, left + 1);
+
+                    if (data[left] > data[right])
+                    {
+                        Swap(data, left, right);
+                    }
+
+                    if (data[left + 1] > data[right])
+                    {
+                        Swap(data, left + 1, right);
+                    }
+
+                    if (data[left] > data[left + 1])
+                    {
+                        Swap(data, left, left + 1);
+                    }
+
+                    int i = left + 1;
+                    int j = right;
+                    int pivot = data[left + 1];
+
+                    while (true)
+                    {
+                        do
+                        {
+                            i++;
+                        }
+                        while (data[i] < pivot);
+                        do
+                        {
+                            j--;
+                        }
+                        while (data[j] > pivot);
+
+                        if (j < i)
+                        {
+                            break;
+                        }
+
+                        Swap(data, i, j);
+                    }
+
+                    data[left + 1] = data[j];
+                    data[j] = pivot;
+
+                    if (j >= k)
+                    {
+                        right = j - 1;
+                    }
+
+                    if (j <= k)
+                    {
+                        left = i;
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+}
---
+++ MedianFinderTests/UnitTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Roadrunner.Interview.MedianFinder;
+
+namespace Roadrunner.Interview.MedianFinder.Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestEasy1()
+        {
+            int[] data = { 1, 2, 3 };
+            int median = MedianFinder.FindMedian(data);
+            Assert.AreEqual(2, median);
+        }
+
+        [TestMethod]
+        public void TestEasy2()
+        {
+            int[] data = { 1, 2, 3, 4 };
+            int median = MedianFinder.FindMedian(data);
+            Assert.AreEqual(3, median);
+        }
+
+        [TestMethod]
+        public void TestMedium1()
+        {
+            int[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int median = MedianFinder.FindMedian(data);
+            Assert.AreEqual(5, median);
+        }
+
+        [TestMethod]
+        public void TestMedium2()
+        {
+            int[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int median = MedianFinder.FindMedian(data);
+            Assert.AreEqual(6, median);
+        }
+
+        [TestMethod]
+        public void TestMedium3()
+        {
+            int[] data = { 5, 2, 11, 4, 2, 6, 7, 10, 2 };
+            int median = MedianFinder.FindMedian(data);
+            Assert.AreEqual(5, median);
+        }
+
+        [TestMethod]
+        public void TestHard1()
+        {
+            int[] data = { 154, 61, 741, 239, 467, 233, 126, 971, 368, 325, 56, 266, 958, 763, 652, 565, 92, 610, 237, 655, 688, 923, 569, 709, 298, 878, 766, 954, 836, 468, 77, 507, 327, 509, 739, 719, 176, 380, 961, 18, 635, 891, 440, 105, 862, 986, 666, 797, 109, 830, 265, 133, 594, 208, 353, 235, 135, 261, 520, 74, 710, 240, 32, 340, 65, 370, 98, 734, 1, 990, 398, 273, 96, 863, 846, 470, 833, 2, 252, 97, 326, 787, 619, 903, 839, 822, 284, 142, 257, 657, 824, 36, 609, 663, 158, 342, 375, 872, 189, 496 };
+            int median = MedianFinder.FindMedian(data);
+            Assert.AreEqual(467, median);
+        }
+    }
+}
